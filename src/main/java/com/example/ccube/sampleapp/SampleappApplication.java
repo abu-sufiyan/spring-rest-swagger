9d8551ba10bd2f856d06e2e6592c4e23a20f2f03
Dev2 <sufiyanrafey@gmail.com>
2021-01-18 00:18:14 +0530

    added for Learning purpose

--- src/main/java/com/example/ccube/sampleapp/SampleappApplication.java
+++ src/main/java/com/example/ccube/sampleapp/SampleappApplication.java
@@ -3,6 +3,8 @@ package com.example.ccube.sampleapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// This is a demo for git HUB.
 @SpringBootApplication
 public class SampleappApplication {
 
